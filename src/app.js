d589356da9091b60146f7e19281badcc817c91e9
Sasank1236 <sasanksudulaguntla@gmail.com>
2026-05-24 23:50:06 +0530

    accountSchema added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
-const authRouter = require('./routes/auth.routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+/**
+ * - Routes Required
+ */
+
+const authRouter = require('./routes/auth.routes');
+const accountRouter = require('./routes/account.routes');
+
+
+/**
+ * - Use Routes
+ */
+
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
 
 app.get("/", (req, res) => {
     res.send("Backend Ledger API is running ");
